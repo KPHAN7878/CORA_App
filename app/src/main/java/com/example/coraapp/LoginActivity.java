7afb6dc7d3c91ab8829bea8e6707c8111c7bf1f4
KPHAN7878 <kevinphan7878@gmail.com>
2021-03-12 18:34:35 -0600

    login/logout implemented

--- app/src/main/java/com/example/coraapp/LoginActivity.java
+++ app/src/main/java/com/example/coraapp/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.coraapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -14,11 +22,15 @@ public class LoginActivity extends AppCompatActivity
     private EditText emailEditText, passwordEditText;
     private Button loginBtn, registerBtn;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
@@ -33,8 +45,69 @@ public class LoginActivity extends AppCompatActivity
                 sendUserToRegisterActivity();
             }
         });
+
+        loginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                AllowUserLogin();
+            }
+        });
     }
 
+    //method for when user clicks login button
+    private void AllowUserLogin()
+    {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        //if email field is empty
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+        }
+        //if password field is empty
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        //if nothing is wrong then sign in
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    //if login is successful
+                    if(task.isSuccessful())
+                    {
+                        sendUserToMainActivity();
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    //if login fails
+                    else
+                    {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(LoginActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    //method for sending user to main page when login is successful
+    private void sendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        //prevent user from pressing back
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    //method for when user clicks reigster button
     private void sendUserToRegisterActivity()
     {
         Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
