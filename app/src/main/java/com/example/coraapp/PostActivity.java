89611201e317828e89f22bcae39d5563deb15cbe
KevinPhan <kevinphan724@gmail.com>
2021-03-15 19:10:49 -0500

    added upload image feature (no database yet)

--- app/src/main/java/com/example/coraapp/PostActivity.java
+++ app/src/main/java/com/example/coraapp/PostActivity.java
@@ -1,14 +1,61 @@
 package com.example.coraapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Button submit_btn_post;
+    private EditText Title_post;
+    private ImageButton picture_post;
+
+    private Uri imageUri = null;
+    private static final int GALLERY_PICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        submit_btn_post = findViewById(R.id.submit_btn_post);
+        Title_post = findViewById(R.id.Title_post);
+        picture_post = findViewById(R.id.picture_post);
+
+        picture_post.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                OpenGallery();
+            }
+        });
+    }
+
+    private void OpenGallery()
+    {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_PICK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && resultCode==RESULT_OK)
+        {
+            imageUri = data.getData();
+            picture_post.setImageURI(imageUri);
+        }
     }
 }
