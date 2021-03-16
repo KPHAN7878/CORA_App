48586fd6f011cc06ddb00b215c8c204dfcaf2a12
KevinPhan <kevinphan724@gmail.com>
2021-03-16 12:20:05 -0500

    master push after merging master with tutorial

--- app/src/main/java/com/example/coraapp/PostActivity.java
+++ app/src/main/java/com/example/coraapp/PostActivity.java
@@ -34,6 +34,7 @@ import java.util.HashMap;
 public class PostActivity extends AppCompatActivity {
 
     //tutorial21 branch test comment
+    //push to master after merging master with tutorial21
 
     private Button submit_btn_post;
     private EditText Title_post;
