f9cf3c31b56127f2e8c7b4fc55036299a0cd1b9c
KPHAN7878 <kevinphan7878@gmail.com>
2021-03-16 19:01:29 -0500

    added navbar

--- app/src/main/java/com/example/coraapp/MainActivity.java
+++ app/src/main/java/com/example/coraapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.coraapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,8 +27,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Button logoutMain;
-    private FloatingActionButton report_btn_main;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView postList;
+    private Toolbar mToolBar;
+
 
     private FirebaseAuth mAuth;
     private DatabaseReference UsersRef;
@@ -31,32 +43,85 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logoutMain = findViewById(R.id.logoutMain);
-        report_btn_main = findViewById(R.id.report_btn_main);
+
+        //adding the toolbar
+        mToolBar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Home");
+
+
+        drawerLayout = findViewById(R.id.draw_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = findViewById(R.id.nav_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+
+        //access navigation_menu.xml in menu folder when clicked
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                GoToNavMenu(item);
+                return false;
+            }
+        });
+
 
         mAuth = FirebaseAuth.getInstance();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
-        //logout button test
-        logoutMain.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                mAuth.signOut();
-                SendUserToLoginActivity();
-            }
-        });
 
-        report_btn_main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                SendUserToPostActivity();
-            }
-        });
     }
 
+
+
+    //action bar hamburger toggle
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    //method that redirects to navigation bar
+    private void GoToNavMenu(MenuItem item)
+    {
+
+        //actions that happen when user clicks on certain navigation buttons
+        switch(item.getItemId())
+        {
+            case R.id.nav_home:
+                Toast.makeText(this, "Home button clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_create:
+                SendUserToPostActivity();
+                break;
+            case R.id.nav_account:
+                Toast.makeText(this, "Account button clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_posts:
+                Toast.makeText(this, "Post button clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                SendUserToLoginActivity();
+                break;
+        }
+    }
+
+
+    //method that goes to post activity
     private void SendUserToPostActivity()
     {
         Intent newReportIntent = new Intent(MainActivity.this, PostActivity.class);
