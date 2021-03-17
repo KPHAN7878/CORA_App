45c602318afbb5a807dc18cfe0e0f76fd969503a
KPHAN7878 <kevinphan7878@gmail.com>
2021-03-17 00:39:42 -0500

    startListening() recycler crash

--- app/src/main/java/com/example/coraapp/MainActivity.java
+++ app/src/main/java/com/example/coraapp/MainActivity.java
@@ -4,17 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,19 +33,26 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
 {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-    private RecyclerView postList;
     private Toolbar mToolBar;
+
+    //recyclerview variables
+    private RecyclerView postList;
+    private FirebaseRecyclerAdapter adapter;
 
 
     private FirebaseAuth mAuth;
     private DatabaseReference UsersRef;
+    //private DatabaseReference PostsRef;
+    private Query PostsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,6 +60,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        //get reference to "Occurrence" node for recycleview method
+
+
+        //PostsRef = FirebaseDatabase.getInstance().getReference().child("Occurrence");
+        //PostsRef = FirebaseDatabase.getInstance().getReference().child("Occurrence");
 
         //adding the toolbar
         mToolBar = findViewById(R.id.main_page_toolbar);
@@ -50,6 +74,7 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().setTitle("Home");
 
 
+        //nav bar stuff
         drawerLayout = findViewById(R.id.draw_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
@@ -57,6 +82,20 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         navigationView = findViewById(R.id.nav_view);
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+
+        //recyclerview for displaying posts
+        postList = findViewById(R.id.all_users_post_list);
+        postList.setLayoutManager(new LinearLayoutManager(this));
+
+        /*
+        postList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //arranging posts from newest to oldest
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        postList.setLayoutManager(linearLayoutManager);
+        */
 
 
         //access navigation_menu.xml in menu folder when clicked
@@ -71,11 +110,93 @@ public class MainActivity extends AppCompatActivity
         });
 
 
-        mAuth = FirebaseAuth.getInstance();
-        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
-
+        DisplayAllPosts();
 
     }
+
+
+    //method that displays all posts
+    //using firebse recycle adapter
+    private void DisplayAllPosts()
+    {
+        super.onStart();
+
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Occurrence");
+
+        FirebaseRecyclerOptions<Posts> options =
+                new FirebaseRecyclerOptions.Builder<Posts>()
+
+                    /*
+                    .setQuery(PostsRef, new SnapshotParser<Posts>()
+
+
+                    {
+                        @NonNull
+                        @Override
+                        public Posts parseSnapshot(@NonNull DataSnapshot snapshot)
+                        {
+                            return new Posts(snapshot.child("FullName").getValue().toString(),
+                                             snapshot.child("UID").getValue().toString(),
+                                             snapshot.child("date").getValue().toString(),
+                                             snapshot.child("image").getValue().toString(),
+                                             snapshot.child("title").getValue().toString());
+                        }
+                    })
+                    */
+                    .setQuery(PostsRef, Posts.class)
+                    .build();
+
+        adapter = new FirebaseRecyclerAdapter<Posts, PostsViewHolder>(options)
+        {
+            @Override
+            public PostsViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.occurrence_post_layout, parent, false);
+
+                return new PostsViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(PostsViewHolder holder, final int position, Posts model)
+            {
+                holder.usersName.setText(model.getFullName());
+                holder.Date.setText(model.getDate());
+                holder.Title.setText(model.getTitle());
+                Picasso.get().load(model.getImage()).into(holder.Image);
+            }
+        };
+
+        postList.setAdapter(adapter);
+
+        //adapter.startListening();
+    }
+
+
+
+
+
+    //static class for "FirebseRecyclerAdapter" in method "DisplayAllPosts"
+    public static class PostsViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView usersName, Date, Title;
+        ImageView Image;
+
+        //constructor
+        public PostsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            usersName = itemView.findViewById(R.id.post_user_name);
+            Date = itemView.findViewById(R.id.post_date);
+            Title = itemView.findViewById(R.id.post_title);
+            Image = itemView.findViewById(R.id.post_image);
+        }
+
+    }
+
+
 
 
 
@@ -129,6 +250,8 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    /* //------------------------------------------------------------------------------------------
+
     //when app is opened make sure that the user is logged in
     @Override
     protected void onStart()
@@ -149,7 +272,10 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    */ // ----------------------------------------------------------------------------------------
 
+
+    /* //------------------------------------------------------------------------------------------
 
     //method that checks if user is in database
     private void CheckUserExistence()
@@ -179,8 +305,9 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
+    */ // ----------------------------------------------------------------------------------------
 
-
+    /* //------------------------------------------------------------------------------------------
 
     //method that sends user to setup activity
     private void SendUserToSetupActivity()
@@ -191,6 +318,7 @@ public class MainActivity extends AppCompatActivity
         finish();
     }
 
+    */ // ----------------------------------------------------------------------------------------
 
 
     //login validation method
---
+++ app/src/main/java/com/example/coraapp/Posts.java
@@ -0,0 +1,83 @@
+package com.example.coraapp;
+
+
+//this class retrieves information for the recycle adapter in MainActivity function "DisplayAllPosts"
+
+
+public class Posts
+{
+    public String FullName, UID, date, image, title;
+
+
+
+    //default constructor
+    public Posts()
+    {
+
+    }
+
+
+
+    //constructors
+    public Posts(String fullName, String UID, String date, String image, String title)
+    {
+        FullName = fullName;
+        this.UID = UID;
+        this.date = date;
+        this.image = image;
+        this.title = title;
+    }
+
+
+
+   //getters and setters
+    public String getFullName()
+    {
+        return FullName;
+    }
+
+    public void setFullName(String fullName)
+    {
+        this.FullName = fullName;
+    }
+
+    public String getUID()
+    {
+        return UID;
+    }
+
+    public void setUID(String UID)
+    {
+        this.UID = UID;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public void setImage(String image)
+    {
+        this.image = image;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+}
