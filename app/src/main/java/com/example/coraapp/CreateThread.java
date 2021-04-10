4721efcb7b513675e148e49a85194d902e320062
KPHAN7878 <kevinphan7878@gmail.com>
2021-04-10 11:12:51 -0500

    -thread page toolbar

---
+++ app/src/main/java/com/example/coraapp/CreateThread.java
@@ -0,0 +1,16 @@
+package com.example.coraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateThread extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_thread);
+    }
+}
--- app/src/main/java/com/example/coraapp/Threads.java
+++ app/src/main/java/com/example/coraapp/Threads.java
@@ -2,13 +2,17 @@ package com.example.coraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Threads extends AppCompatActivity
 {
+    private ImageView back_button, post_button;
+    private TextView toolbar_text;
 
-    TextView test_text_id;
 
     private String testString;
 
@@ -18,11 +22,33 @@ public class Threads extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_threads);
 
-        test_text_id = findViewById(R.id.test_text_id);
+        back_button = findViewById(R.id.back_button);
+        post_button = findViewById(R.id.post_button);
+        toolbar_text = findViewById(R.id.toolbar_text);
 
         /** get string extra from previous */
         testString = getIntent().getExtras().get("Topic").toString();
-        test_text_id.setText(testString);
+
+        back_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent backIntent = new Intent(Threads.this, ForumsHome.class);
+                startActivity(backIntent);
+            }
+        });
+
+        post_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent postIntent = new Intent(Threads.this, CreateThread.class);
+                startActivity(postIntent);
+            }
+        });
+
 
     }
 }
